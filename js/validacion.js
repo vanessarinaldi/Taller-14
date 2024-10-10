b308e40b80b1fde0ee39630a1f9eb3d97fed7afc
Vanessa Rinaldi <vane.rinaldif4@gmail.com>
2024-10-10 20:00:17 -0300

    fix

---
+++ js/validacion.js
@@ -0,0 +1,41 @@
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+let nombre = document.getElementById ("nombre")
+nombre.value === '' 
+
+let apellido = document.getElementById ("apellido")
+apellido.value ==='' 
+
+let password1 = document.getElementById ("password1")
+password1.value ==='' 
+
+let password2 = document.getElementById ("password2")
+password2.value ==='' 
+
+let email = document.getElementById ("email")
+email.value ==='' 
+
+let terminos = document.getElementById ("terminos")
+
+let button = document.getElementById("regBtn")
+
+function Registro() {
+    if (nombre.value !== '' && apellido.value !== '' && password1.value !== '' && password2.value !== '' && email.value !== '' && terminos.checked) {
+        if (password1.value === password2.value && password1.value.length >= 6) {
+            showAlertSuccess();
+        } else {
+            showAlertError();
+        }
+    } else {
+        showAlertError();
+    }
+} 
+
+document.getElementById("regBtn").addEventListener('click', Registro);
+
