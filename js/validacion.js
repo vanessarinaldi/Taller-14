23aa5f0f049d09a8304a8ace56e40e81c7e14901
Vanessa Rinaldi <vane.rinaldif4@gmail.com>
2024-10-26 00:06:07 -0300

    fix

--- js/validacion.js
+++ js/validacion.js
@@ -5,27 +5,15 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
-
-let nombre = document.getElementById ("nombre")
-nombre.value === '' 
-
-let apellido = document.getElementById ("apellido")
-apellido.value ==='' 
-
-let password1 = document.getElementById ("password1")
-password1.value ==='' 
-
-let password2 = document.getElementById ("password2")
-password2.value ==='' 
-
-let email = document.getElementById ("email")
-email.value ==='' 
-
-let terminos = document.getElementById ("terminos")
-
-let button = document.getElementById("regBtn")
-
 function Registro() {
+let nombre = document.getElementById ("nombre");
+let apellido = document.getElementById ("apellido");
+let password1 = document.getElementById ("password1");
+let password2 = document.getElementById ("password2");
+let email = document.getElementById ("email");
+let terminos = document.getElementById("terminosCheckbox");
+
+
     if (nombre.value !== '' && apellido.value !== '' && password1.value !== '' && password2.value !== '' && email.value !== '' && terminos.checked) {
         if (password1.value === password2.value && password1.value.length >= 6) {
             showAlertSuccess();
